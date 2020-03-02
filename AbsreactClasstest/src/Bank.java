62fe9aa92da2e873d5a68811bdfcad3559a87e50
Chandra-2018 <chandraprakashbajagain.com>
2020-03-01 17:00:16 -0700

    2020 game change

--- AbsreactClasstest/src/Bank.java
+++
@@ -1,9 +0,0 @@
-
-public abstract class Bank {
-
-	abstract double getLoneRate();
-	
-	double getExchangeRate() {
-		return 112.67;
-	}
-}
--- AbsreactClasstest/src/Prabhu.java
+++
@@ -1,9 +0,0 @@
-
-public class Prabhu extends Bank{
-
-	@Override
-	double getLoneRate() {
-		return 12.34;
-	}
-
-}
